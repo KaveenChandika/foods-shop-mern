1549e1dba0a489cff497dc836feaa560fdfc5a08
KaveenChandika <ckaveen17@gmail.com>
2021-02-04 21:56:28 +0530

    add heroku url to axios

--- src/components/axios.js
+++ src/components/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance  = axios.create({
-    baseURL:"http://localhost:5000/"
+    baseURL:"https://foods-shop.herokuapp.com/"
 });
 
 export default instance;
